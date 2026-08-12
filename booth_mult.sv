5fd1af3edb8efa85425a0d15caea3f92c722ba8c
Anuj Lall <alall1@ucsc.edu>
2026-08-11 22:33:05 -0700

    added buses for booth_mult, still working on implementation

--- booth_mult.sv
+++ booth_mult.sv
@@ -4,6 +4,9 @@ module booth_mult (
     output logic [7:0] S
 );
 
-
+logic [8:0] reg;    // register that contains accumulator (top 4 bits), Q (next 4 bits) and Q (last bit). Result is bits [8:1] after results
+logic [3:0] acc;    // accumulator, top 4 bits of result
+logic [3:0] Q;      // Q, starts as multiplier B then becomes bottom 4 bits of result
+logic Qlast;        // Qlast, the Q(-1) bit, or the last bit before Q was arithmetically shifted right
 
 endmodule
