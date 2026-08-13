1e03b9d798f9b766056319741f4ef16897dfe547
Anuj Lall <alall1@ucsc.edu>
2026-08-12 20:55:32 -0700

    added implementation of booth_mult, not passing simulation

--- booth_mult.sv
+++ booth_mult.sv
@@ -1,47 +1,46 @@
 module booth_mult (
     input logic clk,
-    input logic [3:0] A,
-    input logic [3:0] B,
-    output logic [7:0] S
+    input logic rst,
+    input logic signed [3:0] M,
+    input logic signed [3:0] Q,
+    output logic signed [7:0] P,
+    output logic done
 );
 
-logic [8:0] r;      // register that contains accumulator (top 4 bits), Q (next 4 bits) and Q (last bit). Result is bits [8:1] after results
-logic [3:0] acc;    // accumulator, top 4 bits of result
-logic [3:0] Q;      // Q, starts as multiplier B then becomes bottom 4 bits of result
-logic Qlast;        // Qlast, the Q(-1) bit, or the last bit before Q was arithmetically shifted right
+logic [8:0] r;              // register that contains accumulator (top 4 bits), Q (next 4 bits) and Q (last bit). Result is bits [8:1] after results
+logic signed [3:0] acc;     // accumulator, top 4 bits of result; signed because acc adds M (or -M) to itself 
+logic [3:0] Qr;             // Qr, starts as multiplier Q inside of r
+logic Qlast;                // Qlast, the Q(-1) bit, or the last bit before Q was arithmetically shifted right
+logic signed [3:0] Mneg;    // -M
 
 logic [1:0] count;  // count of the clock cycles (0, 1, 2, 3)
 
+assign Mneg = -M;   // setting Mneg = -M, would already become signed but already declared as a signed bus
+
 always_comb begin
+    Qr = r[4:1];     // Q will always be equal to 
+    Qlast = r[0];
     case (r[1:0])       // different cases for Qlsb and Qlast
-        2'b00, 2'b11: begin     // only ARS, registers retain values (gets ARS in always_ff)
-            acc = r[8:5];
-            Q = r[4:1];
-            Qlast = r[0];
-        end
-
-        2'b01: begin
-
-        end
-
-        2'b10: begin
-
-        end
-
-        default: begin
-
-        end
-
+        2'b00, 2'b11: acc = r[8:5];     // only ARS, registers retain values (gets ARS in always_ff)
+        2'b01: acc = r[8:5] + Mneg;     // acc = acc + (-M); could have just done acc = acc - M but Mneg to closer match the logic I am implementing
+        2'b10: acc = r[8:5] + M;        // acc = acc + M;
+        default: acc = 4'b0000;         // default case, if r = XXX (Verilator doesn't simulate this)
     endcase
-    acc = count
 end
 
 always_ff @(posedge clk) begin
-    if (count == 2'b11) begin       // count is maxed, stops operations
-        S <= r[8:1];
+    if (rst) begin
+        r <= {4'b0000, Q, 1'b0};
+        count <= 2'b00;
+        done <= 1'b0;
+    end else if (count == 2'b11) begin       // count is maxed, stops operations
+        done <= 1'b1;
     end else begin
-        r <= $signed({acc, Q, Qlast}) >>> 1;    // update r with new acc, Q, Qlast, arithmetic shifted right, because ARS happens at the end of every case
+        r <= $signed({acc, r[4:0]}) >>> 1;    // update r with new acc, Q, Qlast, arithmetic shifted right, because ARS happens at the end of every case
+        count <= count + 1;
     end
 end
+
+assign P = r[8:1];  // constantly assigning S = top 8 bits of R, will be result when "done" is high
 
 endmodule
---
+++ sim_booth_mult.cpp
@@ -0,0 +1,100 @@
+#include "Vbooth_mult.h"    // the C++ class Verilator generated from Verilog; "V" + module name because Verilator prefixes generated class with V (for "Verilated")
+#include "verilated.h"          // core Verilator runtime functions
+#include "verilated_vcd_c.h"    // VCD tracing support, for writing waveform files for GTKWave
+#include <cstdio>               // standard C++ library for printf()
+#include <cstdint>              // standard C++ library for int8_t and uint32_t
+
+uint seed = 1234512;       // seed for Xorshift random number generator
+
+int rand_sign_4bit();
+
+int main(int argc, char** argv) {
+    // handing command-line args to Verilator so flags work
+    Verilated::commandArgs(argc, argv);
+
+    // creating new instance of design; "new" allocates it and returns a pointer "top", which is essentially the handle to the hardware
+    Vbooth_mult* top = new Vbooth_mult;
+
+    // === tracing setup for waveform file ===
+    Verilated::traceEverOn(true);   // globally enable waveform tracing
+    VerilatedVcdC* tfp = new VerilatedVcdC; // create the trace object (the thing that writes the file)
+    top->trace(tfp, 99);            // connect the trace to the design; 99 = how deep into the module hierarchy to record (recording submodule waveforms)
+    
+    tfp->open("booth_mult.vcd"); // open output file to write signals to
+
+    // === variables to use in loop ===
+    vluint64_t sim_time = 0;    // verilator's name for a 64-bit unsigned integer; used as a simulation timestamp that ticks upward
+    int errors = 0;         // a counter for how many test cases fail
+
+    // === helper functions ===
+    auto tick = [&]() {    // tick one clock cycle, one clk cycle = 2 waveform time units
+        top->clk = 0;           // setting clk to 0
+        top->eval();            // evaluating outputs during first half of clk cycle
+        tfp->dump(sim_time++);  // reading current values & incrementing time
+
+        top->clk = 1;           // setting clk to 1
+        top->eval();            // evaluating outputs during second half of clk cycle
+        tfp->dump(sim_time++);  // reading current vals & incrementing time
+    };
+
+    auto testmult = [&](int A, int B) {
+        // set inputs
+        top->M = A;
+        top->Q = B;
+
+        // reset (to clean up testing code, and so "r" picks up the real Q instead of the pre-reset Q)
+        top->rst = 1;
+        tick();
+        top->rst = 0;
+
+        // calculating reference
+        int real_P = A * B;
+        int exp_P = real_P & 0xFF;  // & 0xF because module will return as raw 8 bits
+
+        // ticking 4 times, N-bit multiplication using shift-and-add takes N cycles
+        tick();
+        tick();
+        tick();
+        tick();
+        
+        // comparing & writing
+        if (top->P != exp_P) {
+            printf("ERROR: A=%d B=%d; got P=%d ; expected P=%d real P=%d\n",
+                    A, B, top->P, exp_P, real_P);
+            errors++;
+        } else {
+            printf("OK: A=%d B=%d; got P=%d\n",
+                    A, B, top->P);
+        }
+    };
+
+    // === testing ===
+
+    testmult(rand_sign_4bit(), rand_sign_4bit());
+    testmult(rand_sign_4bit(), rand_sign_4bit());
+    testmult(rand_sign_4bit(), rand_sign_4bit());
+
+    tfp->dump(sim_time++); // taking an extra sample so the last case has visible width
+
+    // === cleanup ===
+    tfp->close();   // close the waveform file so it is written to disk
+    delete top;     // free the memory allocated with "new"
+
+    // === final report ===
+    if (errors == 0) printf("\nAll test cases passed.\n");
+    else printf("\n%d test case(s) failed.\n", errors);
+
+    return errors ? 1 : 0; // return 0 if no errors, or 1 if there were.
+}
+
+// random number generator helper (uses xorshift to generate a number between 0 and 15)
+int rand_sign_4bit () {
+    uint32_t x = seed;
+    x ^= x << 13;
+    x ^= x >> 17;
+    x ^= x << 5;
+    seed = x;
+    int8_t v = x & 0xF;             // low 4 bits, 0..15
+    if (v & 0x8) v |= 0xF0;         // if bit 3 (sign bit of 4-bit twos complement) = 1 → fill upper bits with 1s
+    return v;
+}
