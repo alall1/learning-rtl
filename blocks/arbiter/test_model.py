5627d1444b31c1373c314b45ad051e978b7313bb
Anuj Lall <alall1@ucsc.edu>
2026-09-03 21:44:37 -0700

    confirmed arbiter golden model

--- blocks/arbiter/test_model.py
+++ blocks/arbiter/test_model.py
@@ -1,33 +1,34 @@
 
-# golden model for FIFO buffer
-class arbiter:
+# golden model for fixed-priority arbiter
+class Arbiter:
     def __init__(self, N):
         self.grant = 0
-        self.grant_valid = 0
         self.locked = 0
+        self.N = N
     
     def reset(self):
         self.grant = 0
-        self.grant_valid = 0
         self.locked = 0
 
     def step(self, request_bus, ack):
+        if request_bus > (2 ** (self.N)) - 1: raise ValueError("request_bus out of range")
         if (ack):
             self.locked = 0
-        if request_bus != 0 and locked == 0:
+        if request_bus != 0 and self.locked == 0:
             i = request_bus.bit_length() - 1    # index of granter
             self.grant = 1 << i
             self.locked = 1
 
-# model = Queue(8)
-# print(model.queue)
-# print(model.isEmpty())
-# print(model.isFull())
-# model.write(5)
-# print(model.queue)
-# print(model.isEmpty())
-# print(model.isFull())
-# model.read()
-# print(model.queue)
-# print(model.isEmpty())
-# print(model.isFull())
+model = Arbiter(4)
+print(f"1. grant: {model.grant}, locked: {model.locked}")
+model.step(1, 0)
+print(f"2. grant: {model.grant}, locked: {model.locked}")
+model.step(2, 0)
+print(f"3. grant: {model.grant}, locked: {model.locked}")
+model.step(0, 1)
+print(f"4. grant: {model.grant}, locked: {model.locked}")
+model.step(8, 1)
+print(f"5. grant: {model.grant}, locked: {model.locked}")
+model.step(15, 1)
+print(f"6. grant: {model.grant}, locked: {model.locked}")
+
