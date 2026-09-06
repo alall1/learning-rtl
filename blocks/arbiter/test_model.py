43b475f3da5d92b42ddec72ada0034eaac911a12
Anuj Lall <alall1@ucsc.edu>
2026-09-06 11:08:24 -0700

    finished round_robin_arbiter golden model

--- blocks/arbiter/test_model.py
+++ blocks/arbiter/test_model.py
@@ -1,6 +1,6 @@
 
 # golden model for fixed-priority arbiter
-class Arbiter:
+class fixedArbiter:
     def __init__(self, N):
         self.grant = 0
         self.locked = 0
@@ -20,21 +20,56 @@ class Arbiter:
             self.grant = 1 << i
             self.locked = 1
 
-model = Arbiter(4)
-print(f"1. grant: {model.grant}, locked: {model.locked}")
-model.step(1, 0)
-print(f"2. grant: {model.grant}, locked: {model.locked}")
-model.step(2, 0)
-print(f"3. grant: {model.grant}, locked: {model.locked}")
-model.step(0, 1)
-print(f"4. grant: {model.grant}, locked: {model.locked}")
-model.step(0, 0)
-print(f"5. grant: {model.grant}, locked: {model.locked}")
-model.step(15, 1)
-print(f"6. grant: {model.grant}, locked: {model.locked}")
-model.step(0, 0)
-print(f"7. grant: {model.grant}, locked: {model.locked}")
-model.step(0, 1)
-print(f"8. grant: {model.grant}, locked: {model.locked}")
+# golden model for fixed-priority arbiter
+class roundRobinArbiter:
+    def __init__(self, N):
+        self.grant = 0
+        self.locked = 0
+        self.mask = (2 ** (N)) - 1     # all 1s
+        self.masked = fixedArbiter(N)
+        self.unmasked = fixedArbiter(N)
+        self.N = N
+    
+    def reset(self):
+        self.grant = 0
+        self.locked = 0
+        self.mask = (2 ** (self.N)) - 1
+        self.masked.reset()
+        self.unmasked.reset()
 
+    def step(self, request_bus, ack):
+        print(f"{request_bus:04b}")
+        print(f"{self.mask:04b}")
+        if request_bus > (2 ** (self.N)) - 1: raise ValueError("request_bus out of range")
 
+        if (ack):
+            self.locked = 0
+            self.grant = 0
+
+        self.masked.step(request_bus & self.mask, 1)
+        self.unmasked.step(request_bus, 1)
+
+        grant_masked = self.masked.grant
+        grant_unmasked = self.unmasked.grant
+
+        if self.locked == 0:
+            self.grant = grant_masked if request_bus & self.mask else grant_unmasked
+            self.locked = 1
+
+        self.mask = self.grant - 1
+
+# model = roundRobinArbiter(4)
+# model.step(15, 1)
+# print(f"1. grant: {model.grant}, locked: {model.locked}")
+# model.step(15, 1)
+# print(f"2. grant: {model.grant}, locked: {model.locked}")
+# model.step(12, 1)
+# print(f"3. grant: {model.grant}, locked: {model.locked}")
+# model.step(15, 1)
+# print(f"4. grant: {model.grant}, locked: {model.locked}")
+# model.step(15, 1)
+# print(f"5. grant: {model.grant}, locked: {model.locked}")
+# model.step(15, 1)
+# print(f"6. grant: {model.grant}, locked: {model.locked}")
+# model.step(15, 1)
+# print(f"7. grant: {model.grant}, locked: {model.locked}")
